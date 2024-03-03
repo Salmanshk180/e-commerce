cf1270581575536485b2dcc32801c97432737d08
Salman Shaikh <salmanshaikh7118@gmail.com>
2024-03-03 21:29:52 +0530

    Add Shopping Cards

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../../layout/header/Header'
 import ShopHeader from '../../components/shop-header/ShopHeader'
 import Clients from '../../components/clients/Clients'
+import ShoppingCard from '../../components/shopping-cards/ShoppingCard'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
        <Header/>
        <ShopHeader/>
        <Clients/>
+       <ShoppingCard/>
     </React.Fragment>
   )
 }
