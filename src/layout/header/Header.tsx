05201d0f1291cec6d147108f7a8f7479f3f6d563
Salman Shaikh <salmanshaikh7118@gmail.com>
2024-03-03 20:52:29 +0530

    Add Dark Navbar

---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DarkNavbar from "../../components/navbars/dark-navbar/DarkNavbar";
+
+const Header = () => {
+  return (
+    <React.Fragment>
+      <DarkNavbar background_color="rgb(5, 26, 71)" />
+    </React.Fragment>
+  );
+};
+
+export default Header;
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../layout/header/Header'
+
+const Home = () => {
+  return (
+    <React.Fragment>
+       <Header/>
+    </React.Fragment>
+  )
+}
+
+export default Home
